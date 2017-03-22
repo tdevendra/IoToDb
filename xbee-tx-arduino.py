f283ccabf5a04223a47347f61a094c4df82cbf06
Devendra Tapdia <devendratapdia@gmail.com>
2017-03-22 19:47:56 +0530

    Create xbee-tx-arduino.py

---
+++ xbee-tx-arduino.py
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Test");
+  Serial.print(",");
+  Serial.print("Arduino"); 
+  Serial.print("\n");
+  delay(800);
+}
